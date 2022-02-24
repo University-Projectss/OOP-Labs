1149b898ba3ecaa67ad17189b653ec4f6011dd65
rob3rtu <robertudrea2002@yahoo.ro>
2022-02-24 11:22:37 +0200

    tema0

---
+++ tema0.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+class Numar_Complex {
+private:
+    double real, imaginar;
+
+public:
+    Numar_Complex(double real_, double imaginar_) { //construct init
+        real = real_;
+        imaginar = imaginar_
+    }
+
+    Numar_Complex(Numar_Complex nr_) {              //construct copy
+        real = nr_.real;
+        imaginar = nr_.imaginar;
+    }
+
+    setReal(double real_) {
+        real = real_;
+    }
+
+    setReal(double real_) {
+        real = real_;
+    }
+
+    afisare() {
+        if (real == 0 && imaginar == 0) {
+            cout << 0 << endl;
+        } else if( real == 0 ) {
+            if( imaginar < 0 ) {
+                cout << "-i" << abs(imaginar) << endl;
+            } else {
+                cout << "i" << imaginar << endl;
+            }
+        } else if(imaginar == 0) {
+            cout << real << endl;
+        } else {
+            if( imaginar < 0 ) {
+                cout << real << "-i" << abs(imaginar) << endl;
+            } else {
+                cout << real << "+i" << imaginar << endl;
+            }
+        }
+    }
+
+    getModule() {
+        return sqrt(real*real + imaginar*imaginar);
+    }
+
+    Numar_Complex operator+(Numar_Complex const &obj) {
+        Numar_Complex res;
+        res.real = real + obj.real;
+        res.imaginar = imaginar + obj.imaginar;
+
+        return res;
+    }
+
+    Numar_Complex operator*(Numar_Complex const &obj) {
+        Numar_Complex res;
+
+        res.real = real * obj.real - imaginar * obj.imaginar;
+        res.imaginar = real * obj.imaginar + imaginar * obj.real;
+
+        return res;
+    }
+
+    Numar_Complex operator/(Numar_Complex const &obj) {
+        Numar_Complex res;
+
+        res.real = (real * obj.real + imaginar * obj.imaginar) / (obj.real * obj.real + obj.imaginar * obj.imaginar);
+        res.imaginar = (imaginar * obj.real - real * obj.imaginar) / (obj.real * obj.real + obj.imaginar * obj.imaginar);
+
+        return res;
+    }
+
+};
+
+int main() {
+
+    return 0;
+}
