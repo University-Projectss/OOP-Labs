587bbe8670969bbbb7c75287a68e6d037e22d12e
rob3rtu <robertudrea2002@yahoo.ro>
2022-03-24 20:31:05 +0200

    Circuit v2 acasa

--- Circuit-masini/main.cpp
+++ Circuit-masini/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-
-class Circuit {
-private:
-    
-
-public:
-
-}
 
 class Car {
 private:
@@ -26,25 +20,70 @@ public:
 };
 
 class Dacia : public Car {
+private:
+    string brand = "Dacia";
+
 public:   
     Dacia() : Car() {
         cout << "\nConstructor Dacia\n";
     };
+
+    void getCar() {
+        cout << "\n" << brand << "\n";
+    }
 };
 
-class Toyota : public Car {   
+class Toyota : public Car {
+    string brand = "Toyota";
+
 public:   
     Toyota() : Car() {
         cout << "\nConstructor Toyota\n";
     };
+
+    void getCar() {
+        cout << "\n" << brand << "\n";
+    }
 };
 
 class Renault : public Car {   
+    string brand = "Renault";
+
 public:
     Renault() : Car() {
         cout << "\nConstructor Renault\n";
     };
+
+    void getCar() {
+        cout << "\n" << brand << "\n";
+    }
 };
+
+
+class Circuit {
+private:
+    vector<Car*> v;
+    int n;                  //nr de masini introduse
+
+public:
+    Circuit() {
+        cout << "\nConstructor Circuit\n";
+        n = 0;
+    }
+
+    void addCar(Car* obj) {
+        v.push_back(obj);
+        n++;
+    }
+
+    void showCars() {
+        for(int i = 0; i < n; i++) {
+            cout << v[i]->getSpeed() << '\n';
+        }
+    }
+
+};
+
 
 int main() {
     srand(time(NULL));
@@ -59,6 +98,16 @@ int main() {
 
     Renault c3;
     cout << c3.getSpeed() << '\n';
+
+
+
+    cout << "\n\nAdaugarea masinilor pe Circuit:\n";
+    Circuit C;
+    C.addCar(&c1);
+    C.addCar(&c2);
+    C.addCar(&c3);
+
+    C.showCars();
 
     return 0;
 }
