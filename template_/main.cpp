c0ddb6ce682296bf0dd8bf2a5049e31159e84a31
rob3rtu <robertudrea2002@yahoo.ro>
2022-04-14 11:14:13 +0300

    temoplate?

--- template_/main.cpp
+++ template_/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
+template <typename T = std::string, typename Q = int>
+class MyMap {
+    T nume;
+    Q nota;
+    std::vector<Q>;
 
+public:
+    MyMap() {}
+
+    Q& operator[](const T& nume_) {
+        
+    }
+
+};
 
 int main() {
 
